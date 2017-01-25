15ac87a3eb1edf595cfe05b670e1eb022bc08bdf
ToanNguyen <toan.nguyen@smoovapp.com>
2017-01-25 15:23:35 +0700

    Commit source code

---
+++ src/FishMarket/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FishMarket.Entities;
+using FishMarket.Interfaces;
+using FishMarket.BLO;
+using AutoMapper;
+
+namespace FishMarket.Controllers
+{
+    public class ProductController : BaseController
+    {
+        private readonly IUserDataService _userDataService;
+        private readonly IMapper _mapper;
+        public ProductController(IUserDataService userDataService, IMapper mapper)
+        {
+            _userDataService = userDataService;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(MembershipModel membershipModel)
+        {
+            UserBussinessService userBussinessService = new UserBussinessService(_userDataService, _mapper);
+            TransactionalInformation transaction;
+            Membership membership  = userBussinessService.Login(membershipModel, out transaction);
+            return View();
+        }
+
+        public IActionResult ListProduct()
+        {
+    
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
